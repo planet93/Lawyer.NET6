8d3e3d69559989b8967ac29920767ac6367b28fb
sergey.ermolaev <sergey.ermolaev@base4web.ru>
2022-08-22 17:38:13 +0300

    first commit

---
+++ src/Lawyer.NET6.WEB/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Lawyer.NET6.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lawyer.NET6.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Connect()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Lawyer.NET6.WEB/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿var vueDefaultId = "#globalSection";
+var loader = document.getElementById("loader");
+var emptyString = "";
+
+loader.hide = function () {
+    this.style.display = 'none';
+}
+
+loader.show = function () {
+    this.style.display = '';
+}
